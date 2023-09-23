cd4690edf9e00d236f63ad7decee6b827eadcf85
Pundima Lakshan <pundimal@gmail.com>
2023-09-23 18:23:50 +0530

    add fade

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { Fade } from 'react-awesome-reveal';
 import {
   Hero,
   Footer,
@@ -14,15 +15,17 @@ import {
 function App() {
   return (
     <div className="bg-gradient-to-r from-slate-800 to-slate-900 text-white overflow-hidden">
-      <Hero />
-      <Ridma />
-      <VocalArtistsCircles />
-      {/* <VocalArtistsIndividual /> */}
-      <Orchestra />
-      <Sithpawura />
-      <PartnersSection />
-      <ContactUs />
-      <Footer />
+      <Fade>
+        <Hero />
+        <Ridma />
+        <VocalArtistsCircles />
+        {/* <VocalArtistsIndividual /> */}
+        <Orchestra />
+        <Sithpawura />
+        <PartnersSection />
+        <ContactUs />
+        <Footer />
+      </Fade>
     </div>
   );
 }
