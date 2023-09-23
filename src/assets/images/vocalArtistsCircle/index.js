c90da1ad91f8b7c2006a7ca9bffe91e4603fc8b7
Pundima Lakshan <pundimal@gmail.com>
2023-09-23 17:33:54 +0530

    delete all

--- src/assets/images/vocalArtistsCircle/index.js
+++
@@ -1,4 +0,0 @@
-import Singer1 from './Singer1.png';
-import Singer2 from './Singer2.png';
-
-export const vocalArtistsCircle = [Singer1, Singer2];
--- src/assets/images/vocalArtistsIndividual/index.js
+++
@@ -1,3 +0,0 @@
-import S1 from './S1.png';
-
-export const vocalArtistsIndividual = [S1];
